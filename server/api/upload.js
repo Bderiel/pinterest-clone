57cd1ffa7b0e903ae701c7dc074b3fe3aedbbf47
Bderiel <bderiel@gmail.com>
2018-01-07 00:11:14 -0500

    feat(s3 upload works damn group policies)

--- server/api/upload.js
+++ server/api/upload.js
@@ -1,5 +1,16 @@
 const router = require('express').Router();
 const multer = require('multer');
+const AWS = require('aws-sdk');
+
+const s3 = new AWS.S3();
+
+AWS.config.update(
+  {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    subregion: 'us-east-1',
+  });
+
 
 const upload = multer({
   storage: multer.memoryStorage(),
@@ -7,10 +18,24 @@ const upload = multer({
   limits: { fileSize: 52428800 },
 });
 
-router.post('/', upload.single('photo'), (req, res, next) => {
-  console.log(process.env.amazons3, 'test');
-
-  res.send(process.env.amazons3);
+router.post('/', upload.single('photo'), (req, res) => {
+  const params = {
+    Bucket: 'brian-pinterest-clone',
+    Key: req.file.originalname,
+    Body: req.file.buffer,
+    ACL: 'public-read', // your permisions
+  };
+  s3.putObject(params, (err, data) => {
+    if (err) {
+      console.log(err);
+      res.status(400).send(err.message);
+    } else {
+      res.json({
+        status: 'success',
+        url: `https://s3.amazonaws.com/brian-pinterest-clone/${req.file.originalname}`,
+      });
+    }
+  });
 });
 
 module.exports = router;
